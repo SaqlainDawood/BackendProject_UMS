0b6dd23de9499ee0c90b006f7af46fe44c920863
Saqlain Dawood <saqlaindawood123@gmail.com>
2026-02-08 08:37:07 -0800

    Update Email

--- utils/FREmail.js
+++ utils/FREmail.js
@@ -18,14 +18,15 @@ export const sendFacultyEmail = async (data) => {
   console.log("Email recipient:", to);
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+   host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
-await transporter.verify();
-console.log("✅ Gmail SMTP connected successfully");
+
 
   // Login Link
   const loginURL = process.env.Front_END_URL+'admin/dashboard/faculty/login';
