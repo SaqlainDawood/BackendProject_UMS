bffb64f033cfc428f7f60619af467cef6b86412f
Saqlain Dawood <saqlaindawood123@gmail.com>
2026-02-16 22:52:26 -0800

    class model fix

--- Models/CreateClass.js
+++ Models/CreateClass.js
@@ -11,7 +11,7 @@ const classSchema = new mongoose.Schema({
     },
     semester:{
         type:Number,
-        requreid:true,
+        required:true,
     },
     teacher:{
         type:mongoose.Schema.Types.ObjectId,
