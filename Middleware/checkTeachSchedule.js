a2bc7b5a7ec6d18ba41a945b6c1bc1acefe992fe
Saqlain Dawood <saqlaindawood123@gmail.com>
2026-03-05 00:41:06 -0800

    schedule logic

---
+++ Middleware/checkTeachSchedule.js
@@ -0,0 +1,26 @@
+import CreateClass from "../Models/CreateClass";
+
+
+export const checkTeacherSchedule = async(req,res,next)=>{
+    try {
+        const {teacherId , day , startTime} = req.body;
+        const conflict = await CreateClass.findOne({
+            "teachers.teacher":teacherId,
+            "schedule.day":day,
+            "schedule.startTime":startTime
+        })
+        if(conflict){
+            return res.status(400).json({
+                success:false,
+                message:"Teacher already has class at this Time!!!!"
+            })
+        }
+        next();
+    } catch (error) {
+        console.log("Teacher Schedule Conflict Error",error);
+        res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
--- Routes/AdminClassRoutes/AdminClassesCRUD.js
+++ Routes/AdminClassRoutes/AdminClassesCRUD.js
@@ -6,11 +6,12 @@ import {
 
 } from "../../Controllers/Admin/AdminClassCRUD/AdminClassCRUD.js";
 import { getTeacherSchedule } from "../../Controllers/Admin/TeacherSchedule/TecherSchedule.js";
+import { checkTeacherSchedule } from "../../Middleware/checkTeachSchedule.js";
 
 const router = express.Router();
 router.get("/all", getAllClasses);
 router.get("/faculty/:teacherId/schedule", getTeacherSchedule);
-router.post("/create", createClass);
+router.post("/create", checkTeacherSchedule, createClass);
 router.get("/:id", getSingleClass);
 // router.put("/:id", updateClass);
 // router.delete("/:id", deleteClass);
